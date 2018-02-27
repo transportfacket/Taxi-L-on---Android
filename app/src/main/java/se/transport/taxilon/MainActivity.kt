4564f759d6513dc0f45eb478f2f8a35bdb660614
Patrik Persson <patrik.persson@transport.se>
2018-02-27 21:30:31 +0100

    more added

--- app/src/main/java/se/transport/taxilon/MainActivity.kt
+++ app/src/main/java/se/transport/taxilon/MainActivity.kt
@@ -4,21 +4,43 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
+
+import android.view.animation.AnimationUtils
+
 
 
 
 class MainActivity : AppCompatActivity() {
 
+
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 
 
 
     fun calculatorButtonOnClick(v: View) {
+
         val intent = Intent(this, CalculatorActivity::class.java)
+        startActivity(intent)
+    }
+
+
+    fun monthlyButtonOnClick(v: View) {
+
+        val intent = Intent(this, MonthlyActivity::class.java)
+        startActivity(intent)
+    }
+
+
+    fun faqButtonOnClick(v: View) {
+
+        val intent = Intent(this, FaqActivity::class.java)
         startActivity(intent)
     }
 }
---
+++ app/src/main/java/se/transport/taxilon/ShowResultActivity.kt
@@ -0,0 +1,7 @@
+package se.transport.taxilon
+
+/**
+ * Created by transport on 2018-02-27.
+ */
+class ShowResultActivity {
+}
