2e1adaece8a183396720b21a36945142a3dad3c9
Patrik Persson <patrik.persson@transport.se>
2018-03-01 16:05:19 +0100

    Much more changes

--- app/src/main/java/se/transport/taxilon/CalculatorActivity.kt
+++ app/src/main/java/se/transport/taxilon/CalculatorActivity.kt
@@ -1,14 +1,32 @@
 package se.transport.taxilon
 
+
+import android.Manifest
+import android.annotation.SuppressLint
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.media.MediaPlayer
+import android.net.Uri
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
+import android.support.v7.app.AlertDialog
 import android.support.v7.widget.Toolbar
+import android.util.Log
 import android.view.View
 import android.widget.*
-import kotlinx.android.synthetic.main.activity_calculator.*
+import android.widget.RadioGroup
+
+
+
 
 class CalculatorActivity : AppCompatActivity() {
+
+
+
+
+    var TAG: String = ""
 
 
     val garantiLonSthml = 19572
@@ -22,21 +40,93 @@ class CalculatorActivity : AppCompatActivity() {
     var lon = 0
     var realLon = 0
 
-
+    var percentageOfFullTime = 0
+    lateinit var kollektivYes : RadioButton
+    lateinit var kollektivNo : RadioButton
+    lateinit var seekBar : SeekBar
+    lateinit var currentPercentage : TextView
+    lateinit var etLocation : EditText
+    lateinit var etWorkedHours : EditText
+    lateinit var workedYes: RadioButton
+    lateinit var kollektivGroup: RadioGroup
+    lateinit var calculateButton: Button
+    var hasCollectiveAgreement: Boolean = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calculator)
         setupActionBar()
 
+        kollektivYes = findViewById(R.id.radioButtonYes)
+        kollektivNo = findViewById(R.id.radioButtonNo)
+        seekBar = findViewById(R.id.seekBar)
+        currentPercentage = findViewById(R.id.tvCurrentPercentage)
+        etLocation = findViewById(R.id.etLocation)
+        etWorkedHours = findViewById(R.id.etWorkedHours)
+        calculateButton = findViewById(R.id.bBerakna)
 
 
-        val kollektivYes = findViewById<RadioButton>(R.id.radioButtonYes) as RadioButton
-        val kollektivNo = findViewById<RadioButton>(R.id.radioButtonNo) as RadioButton
-        val calculateButton = findViewById<Button>(R.id.bBerakna) as Button
+        seekBar()
+
+        radioGroupCollectiveAgreement()
+        }
+
+
+
+    fun seekBar(){
+
+        seekBar.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(p0: SeekBar?, progress: Int, p2: Boolean) {
+                percentageOfFullTime = progress
+                val percentageText = percentageOfFullTime.toString() + "%"
+                currentPercentage.text = percentageText
+
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+
+            }
+        })
+
+
+
 
 
     }
+
+
+    fun radioGroupCollectiveAgreement(){
+        kollektivGroup = findViewById<View>(R.id.rgCollectiveAgreement) as RadioGroup
+        kollektivGroup.setOnCheckedChangeListener(object : RadioGroup.OnCheckedChangeListener {
+            override fun onCheckedChanged(group: RadioGroup, checkedId: Int) {
+
+
+
+                when(checkedId){
+                    R.id.radioButtonYes ->{
+                        hasCollectiveAgreement = true
+                        calculateButton.setBackgroundResource(R.drawable.button_bg_rounded_corners)
+
+                    }
+                    R.id.radioButtonNo ->{
+                       hasCollectiveAgreement = false
+                        calculateButton.setBackgroundResource(R.drawable.button_bg_rounded_corners_grey)
+                        alertDialog()
+
+                    }
+                }
+
+
+
+
+            }
+        })
+    }
+
 
 
 
@@ -44,7 +134,7 @@ class CalculatorActivity : AppCompatActivity() {
         val toolBar = findViewById<Toolbar>(R.id.app_bar) as Toolbar
         toolBar.title=""
         setSupportActionBar(toolBar)
-        var tvToolBar = findViewById<TextView>(R.id.tvToolbarTitle) as TextView
+        val tvToolBar = findViewById<TextView>(R.id.tvToolbarTitle) as TextView
         tvToolBar.text = getString(R.string.calculatorButtonString)
 
 
@@ -56,14 +146,99 @@ class CalculatorActivity : AppCompatActivity() {
 
     }
 
-
     fun backButton(v: View){
         onBackPressed()
     }
 
 
+    fun calculatorButton(v: View){
+
+        when(hasCollectiveAgreement){
+
+            true -> {
+
+                calculateSalaryBy(whereDoIWork(etLocation.text.toString()), etWorkedHours.text.toString().toInt(), percentageOfFullTime, true )
+            }
+
+            false -> {
+
+            }
+
+        }
 
 
+
+
+    }
+
+
+
+    @SuppressLint("MissingPermission")
+    fun alertDialog (){
+
+
+        val builder: AlertDialog.Builder
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            builder = AlertDialog.Builder(this)
+        } else {
+            builder = AlertDialog.Builder(this)
+        }
+        builder.setTitle("Inget kollektivavtal - Ingen garantilön")
+                .setCancelable(false)
+                .setMessage("Denna app utvecklades av Patrik Persson på Transports avdelning 12 i Malmö")
+                .setPositiveButton("OK", { _, _ ->
+
+
+                })
+                .setNeutralButton("Ring Transport", { _, _ ->
+                    setupPermissions()
+                    val intent = Intent(Intent.ACTION_CALL)
+                    intent.data = Uri.parse("tel:0104803000")
+                    startActivity(intent)
+
+
+                })
+
+
+
+                .show()
+
+    }
+
+
+
+    private fun setupPermissions() {
+        val permission = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.CALL_PHONE)
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            Log.i(TAG, "Permission to record denied")
+        }
+    }
+
+
+
+    fun whereDoIWork(location: String): Boolean {
+
+    val location2 = location.toLowerCase().trim()
+
+        if (location2 == "sthlm" || location2 == "stockholm" || location2 == "stokholm" || location2 == "stocholm" ) {
+
+            return true
+        }
+        return false
+    }
+
+
+    fun calculateSalaryBy(isLocationSthlm: Boolean, workedHours: Int, percentageOfFulltime: Int, workAllWeekDays: Boolean): Int{
+
+
+
+
+
+        return 0
+
+    }
 
 
 }
--- app/src/main/java/se/transport/taxilon/MainActivity.kt
+++ app/src/main/java/se/transport/taxilon/MainActivity.kt
@@ -5,42 +5,84 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 
-import android.view.animation.AnimationUtils
 
-
+import android.os.Build
+import android.support.v7.app.AlertDialog
+import android.media.MediaPlayer
+import android.widget.Button
+import android.widget.ImageView
 
 
 class MainActivity : AppCompatActivity() {
 
-
-
-
+    lateinit var calculatorButton: Button
+    lateinit var monthlyButton: Button
+    lateinit var faqButton: Button
+    lateinit var logoButton: ImageView
+    var tapCount = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        calculatorButton = findViewById(R.id.calculatorButton)
+        monthlyButton = findViewById(R.id.monthlyButton)
+        faqButton = findViewById(R.id.faqButton)
+        logoButton = findViewById(R.id.logo)
+
+        calculatorButton.setOnClickListener{
+            val intent = Intent(this, CalculatorActivity::class.java)
+            startActivity(intent)
+        }
+
+        monthlyButton.setOnClickListener {
+            val intent = Intent(this, MonthlyActivity::class.java)
+            startActivity(intent)
+        }
+
+        faqButton.setOnClickListener {
+            val intent = Intent(this, FaqActivity::class.java)
+            startActivity(intent)
+        }
+
+
+        logoButton.setOnClickListener {
+            if (tapCount < 11) {
+                tapCount += 1
+            } else {
+                alertDialog()
+                tapCount = 0
+            }
+        }
+
     }
 
 
 
-    fun calculatorButtonOnClick(v: View) {
 
-        val intent = Intent(this, CalculatorActivity::class.java)
-        startActivity(intent)
+
+
+
+
+
+    fun alertDialog (){
+        val mPlayer = MediaPlayer.create(this, R.raw.audio)
+        mPlayer.start()
+
+        val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+        builder.setTitle("Du hittade hit!")
+                .setCancelable(false)
+                .setMessage("Denna app utvecklades av Patrik Persson på Transports avdelning 12 i Malmö")
+
+                .setNeutralButton(android.R.string.yes, { _, _ ->
+                    mPlayer.stop()
+
+                })
+
+
+
+                .show()
+
     }
 
-
-    fun monthlyButtonOnClick(v: View) {
-
-        val intent = Intent(this, MonthlyActivity::class.java)
-        startActivity(intent)
-    }
-
-
-    fun faqButtonOnClick(v: View) {
-
-        val intent = Intent(this, FaqActivity::class.java)
-        startActivity(intent)
-    }
 }
